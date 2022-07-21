bc19535b2e5bb3a266b78cf2589e49c34b3bb51b
Rodrigo Tavares <rtsantana@outlook.com.br>
2022-07-21 13:49:32 -0300

    organizing code in Page Objectics

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -1,15 +1,9 @@
-beforeEach(() => {
-  cy.viewport(1440, 900)
-})
+import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro', ()=>{
+
   it('Cadastro de um entregador', ()=> {
-    cy.visit('https://buger-eats.vercel.app')
-    //Localizador CSS para encontrar o botão para clicar 
-    cy.get('a[href="/deliver"]').click()
-    // Checando se realmente está na página de cadastro com um Texto existente na mesma
-    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-    // Criando uma massa de dados para o cenário feliz de preenchimento dos campos
+
     var deliveryman = {
       name: 'Rodrigo Tavares',
       cpf: '00011122233',
@@ -31,49 +25,28 @@ describe('Cadastro', ()=>{
         cnh: 'cnh-digital.jpg'
       }
     }
-    cy.get('input[name="name"]').type(deliveryman.name)
-    cy.get('input[name="cpf"]').type(deliveryman.cpf)
-    cy.get('input[name="email"]').type(deliveryman.email)
-    cy.get('input[name="whatsapp"]').type(deliveryman.whatsapp)
 
-    cy.get('input[name="postalcode"]').type(deliveryman.address.postalcode)
-    cy.get('input[type=button][value="Buscar CEP"]').click()
-
-    cy.get('input[name="address-number"]').type(deliveryman.address.number)
-    cy.get('input[name="address-details"]').type(deliveryman.address.details)
-    //Confirmando os dados preenchidos com a massa de teste
-    cy.get('input[name="address"]').should('have.value', deliveryman.address.street)
-    cy.get('input[name="district"]').should('have.value', deliveryman.address.district)
-    cy.get('input[name="city-uf"]').should('have.value', deliveryman.address.city_uf)
-    //Buscando um elemento pelo texto através da 'CONTAINS'
-    //CONSTAINS recebe o localizador (Juntar um localizador CSS com um texto que existe na página)
-    cy.contains('.delivery-method li', deliveryman.delivery_method.third_method).click()
-
-    //Inserindo a imagem da CNH no campo com drag-drop
-    cy.get('div[class="dropzone"]').selectFile('cypress/fixtures/images/cnh-digital.jpg', {action: 'drag-drop',})
-
-    cy.get('form button[type="submit"]').click()
+    var signup = new SignupPage()
+    
+    signup.go()
+    signup.fillForm(deliveryman)
+    signup.submit()
 
     const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-    cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+    signup.modalContentShouldBe(expectedMessage)
 
     cy.get('button.swal2-confirm').click()
   })
 
   it('Cadastro de um entregador com CPF errado', ()=> {
-    cy.visit('https://buger-eats.vercel.app')
-    //Localizador CSS para encontrar o botão para clicar 
-    cy.get('a[href="/deliver"]').click()
-    // Checando se realmente está na página de cadastro com um Texto existente na mesma
-    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-    // Criando uma massa de dados para o cenário feliz de preenchimento dos campos
+
     var deliveryman = {
       name: 'Rodrigo Tavares',
       cpf: '000111222AS',
       email: 'email@hotmail.com',
       whatsapp: '11999999999',
       address: {
-        cep: '05894370',
+        postalcode: '05894370',
         street: 'Rua Manoel da Conceição',
         number: '14',
         details: 'Macedônia',
@@ -88,30 +61,11 @@ describe('Cadastro', ()=>{
         cnh: 'cnh-digital.jpg'
       }
     }
-    cy.get('input[name="name"]').type(deliveryman.name)
-    cy.get('input[name="cpf"]').type(deliveryman.cpf)
-    cy.get('input[name="email"]').type(deliveryman.email)
-    cy.get('input[name="whatsapp"]').type(deliveryman.whatsapp)
+    var signup = new SignupPage
 
-    cy.get('input[name="postalcode"]').type(deliveryman.address.cep)
-    cy.get('input[type=button][value="Buscar CEP"]').click()
-
-    cy.get('input[name="address-number"]').type(deliveryman.address.number)
-    cy.get('input[name="address-details"]').type(deliveryman.address.details)
-    //Confirmando os dados preenchidos com a massa de teste
-    cy.get('input[name="address"]').should('have.value', deliveryman.address.street)
-    cy.get('input[name="district"]').should('have.value', deliveryman.address.district)
-    cy.get('input[name="city-uf"]').should('have.value', deliveryman.address.city_uf)
-    //Buscando um elemento pelo texto através da 'CONTAINS'
-    //CONSTAINS recebe o localizador (Juntar um localizador CSS com um texto que existe na página)
-    cy.contains('.delivery-method li', deliveryman.delivery_method.third_method).click()
-
-    //Inserindo a imagem da CNH no campo com drag-drop
-    cy.get('div[class="dropzone"]').selectFile('cypress/fixtures/images/cnh-digital.jpg', {action: 'drag-drop',})
-
-    cy.get('form button[type="submit"]').click()
-
-    //Buscando a informação de errro do CPF inválido
-    cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+    signup.go()
+    signup.fillForm(deliveryman)
+    signup.submit()
+    signup.alerMessageShouldBe('Oops! CPF inválido')
   })
 })
